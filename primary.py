9e0c8579141951b83727708e7c4db42a7897cd9e
Mya Walker <86728946+MW-2005@users.noreply.github.com>
2021-07-16 14:04:14 +0000

    answers

--- primary.py
+++ primary.py
@@ -1,5 +1,5 @@
-# author:
-# date:
+# author: Mya Walker
+# date: 7/13/21
 
 # --------------- Section 1 --------------- #
 
@@ -12,12 +12,41 @@
 #   d. Print the variable capitalized (Look to documentation.)
 
 
+name= ' Mya '
+print(name.center(30))
+
+print(name.upper())
+print(name.lower())
+
+#################print(.upper())
+
+
+
 # 2 | String Methods
 #
 # 1 - Prompt input from the user, asking them to enter a sentence. Save it to a variable named text.
 #   a. Find the first instance of the letter a. Print that position.
 #   b. Find the first instance of the letter b. Print that position.
 #   c. Find the first instance of a word of your choice. Print that position.
+
+print()
+print()
+print()
+
+
+text=(input('Enter a sentence: '))
+
+print(text)
+
+print('position of the first \'t\':', text.find('t'))
+print()
+print('position of the first \'h\':', text.find('h'))
+print()
+print('position of the first \'e\':', text.find('e'))
+
+
+
+
 
 
 # 3 | String Methods
@@ -27,6 +56,7 @@
 #   b. Using a built-in function, print the position of the vowel that shows up last.
 #   c. Using a built-in function, print the position of the vowel that shows up first.
 
+text(input('enter a new sentence: ')
 
 # 4 | String Indexing
 #
